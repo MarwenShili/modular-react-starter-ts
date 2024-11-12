3f1be06de69cead931208987d11b02661277e34c
marwenshili <eshilimarwen9@gmail.com>
2024-11-12 16:16:07 +0100

    remove unused page

--- src/modules/shared/store/rootReducer.ts
+++ src/modules/shared/store/rootReducer.ts
@@ -1,13 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import themeReducer from './slices/theme/themeSlice'
 import authReducer from '../../auth/data/authSlice'
-import bookReducer from '../../book/data/bookSlice'
 import modalReducer from './slices/modal/modalSlice'
 
 const rootReducer = combineReducers({
   theme: themeReducer,
   auth: authReducer,
-  book: bookReducer,
   modal: modalReducer,
 })
 
